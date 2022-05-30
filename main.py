5fb8938ed6fc2e03812e62461c3201d1ef6b7ca8
nika <nika.namoradze.1@btu.edu.ge>
2022-05-30 19:53:35 +0400

    registration commit

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'URCHIE_KEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urchie.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    first_name = db.Column(db.String, primary_key=True, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    e_mail = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    user_image = db.Column(db.LargeBinary, nullable=False)
+
+    def __str__(self):
+        return f"first_name:{self.first_name}; last_name:{self.last_name}; age:{self.age}; e_mail:{self.e_mail}; password:{self.password};"
+
 
 @app.route('/')
 def home():
@@ -12,8 +33,24 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/registration')
+@app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        age = request.form['age']
+        e_mail = request.form['email']
+        password = request.form['password']
+        user_image = request.files['image']
+
+        user = users(first_name=first_name, last_name=last_name, age=age, e_mail=e_mail, password=password,
+                     user_image=user_image.read())
+        db.session.add(user)
+        db.session.commit()
+        return render_template('index.html')
+
+        # return f"first_name:{first_name}; last_name:{last_name}; age:{age}; e_mail:{e_mail}; password:{password}"
+
     return render_template('registration.html')
 
 
@@ -32,8 +69,7 @@ def add():
     return render_template('add.html')
 
 
-#just a comment
-
+# just a comment
 
 
 if __name__ == '__main__':
