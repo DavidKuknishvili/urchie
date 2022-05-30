a8dca870b3a6721f5f4139faa86d8cb7a1e8f25f
DavidKuknishvili <dato.kuknishvili.1@btu.edu.ge>
2022-05-30 18:02:42 +0400

    URCHIE_V0.1

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
+@app.route('/open')
+def open():
+    return render_template('open.html')
+
+
+@app.route('/first')
+def first():
+    return render_template('first.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
